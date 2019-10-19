96dd54e1353be8ad2a03179285a511f2701969a1
NotInTheSubject <sergeyb.2102@gmail.com>
2019-10-19 18:33:43 +0300

    Rename the field of the Pipeline class

--- src/pipeline.hpp
+++ src/pipeline.hpp
@@ -24,18 +24,18 @@ public:
     using config_type = std::map<CONFIGTYPE, std::any>;
 
 private:
-    void *state;
+    void *state_storage;
     config_type const* config;
 public:
     Pipeline(config_type const*);
     Pipeline() noexcept;
     ~Pipeline();
 
-    bool is_full_conf() noexcept;
-    void dump(std::ostream&);
+    bool is_full_conf() const noexcept;
+    void dump(std::ostream&) const;
 
     void run_process() noexcept;
     void set_config(config_type const*);
-    config_type const* get_config() noexcept;
+    config_type const& get_config() const noexcept;
 };
 
