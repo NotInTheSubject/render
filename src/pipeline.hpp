db091cc0cfe244bd1be84e403fbeada3db783f37
NotInTheSubject <sergeyb.2102@gmail.com>
2019-10-19 15:44:24 +0300

    Changed pipeline declaration

--- src/pipeline.hpp
+++ src/pipeline.hpp
@@ -1,11 +1,18 @@
 #pragma once
 #include <map>
-#include "frame_types.hpp"
+#include <any>
+#include <ostream>
+
+enum class CONFIGTYPE {
+    DEPTH_FRAME,
+    ENDFRAME,
+    RESOLUTION_FOR_DRAWING,
+};
 
 class Pipeline
 {
 public:
-    using config_type = std::map<std::string, void*>;
+    using config_type = std::map<CONFIGTYPE, std::any>;
 
 private:
     void *state;
@@ -14,6 +21,9 @@ public:
     Pipeline(config_type const*);
     Pipeline() noexcept;
     ~Pipeline();
+
+    bool is_full_conf() noexcept;
+    void dump(std::ostream&);
 
     void run_process() noexcept;
     void set_config(config_type const*);
